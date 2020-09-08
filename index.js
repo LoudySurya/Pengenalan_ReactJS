e55614a31dae74831441721c7d46e63b5cb6e4cd
LoudySurya <63911353+LoudySurya@users.noreply.github.com>
2020-09-08 13:04:10 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  import React, {Component} from 'react';  
+import './App.css';  
+  
+class App extends Component {  
+  render(){  
+    return (  
+      <div className="App container">  
+        <div className="alert alert-info">  
+          <h3 className="text-danger">Ini Project Pertama React JS</h3>  
+          <p>Belajar React JS itu mudah</p>  
+          <button className="mr-1 btn btn-success">Setuju</button>  
+          <button className="btn btn-info">Iya Dong</button>  
+        </div>  
+      </div>  
+    );  
+  }  
+}  
+export default App;  
+
+  
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
